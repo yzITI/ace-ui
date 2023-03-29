1b7f2deb478b02e1f6147eec24c3a9ce63c41639
Phantomlsh <phantomlsh@outlook.com>
2023-03-29 13:56:05 -0700

    Complete basic AModal

--- src/lib/AButton.svelte
+++ src/lib/AButton.svelte
@@ -36,7 +36,7 @@
   button.a-shadow {
     box-shadow: 0 1px 3px 0 rgb(0 0 0 / 0.1), 0 1px 2px -1px rgb(0 0 0 / 0.1);
   }
-  button.a-shadow:hover {
+  button.a-shadow:hover { /* bigger shadow */
     box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
   }
   button.a-border {
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -11,5 +11,13 @@
   <AButton style="margin: 1rem;" className="a-border" color="#00f" background="#eef">border button</AButton>
 </div>
 
-<AModal bind:show={show}></AModal>
-<AceButton></AceButton>
+<div style="border: red 1px solid; height: 500px; width: 100%; position: relative;">
+  <AModal bind:show={show} className="a-left a-absolute">
+    <div style="background: white; min-width: 100%; min-height: 100%; padding: 2rem; box-sizing: border-box;">
+      {#each (new Array(20)) as k}
+        <h1>hi!</h1>
+      {/each}
+      <p>hi!</p>
+    </div>
+  </AModal>
+</div>
