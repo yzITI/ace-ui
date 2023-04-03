49270f856e821ed4ee8c99218b430ea2da4247f9
Phantomlsh <phantomlsh@outlook.com>
2023-04-02 19:37:50 -0700

    Update ASwitch.svelte

--- src/lib/A/ASwitch.svelte
+++ src/lib/A/ASwitch.svelte
@@ -6,9 +6,13 @@
   export let style = '' // button style
   export let activeColor = 'rgb(59 130 246)'
   export let inactiveColor = '#999'
+
+  function toggle () {
+    value = !value
+  }
 </script>
 
-<div class="a-switch-outer" style:width={`calc(2 * ${size})`} on:click={() => value = !value} style:padding={`calc(${size} / 10)`} style:background={value ? activeColor : inactiveColor}>
+<div class="a-switch-outer" style:width={`calc(2 * ${size})`} on:click={toggle} on:keyup={toggle} style:padding={`calc(${size} / 10)`} style:background={value ? activeColor : inactiveColor}>
   <div style:border-width={`calc(${size} / 12)`} style:left={value ? '50%' : '0%'} style:width={size} style:height={size} class={'a-switch-inner ' + className} style={style}></div>
 </div>
 
