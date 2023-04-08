24f5ec46a7197b154e5303855ce14d8cc58b9a83
Tina Ji <z5191511@ad.unsw.edu.au>
2023-04-09 08:48:07 +1000

    update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Ace UI</h1>
-<p>A UI Library for Svelte</p>
+<script>
+  import logo from '$assets/logo.svg'
+  import { AButton, AIcon } from '$lib'
+  import { mdiGithub } from '@mdi/js'
+
+</script>
+<div>
+  <img src={logo} alt="logo" height="400px">
+  <h1>Ace UI</h1>
+  <p>A UI Library for Svelte</p>
+</div>
+<div>
+  <AButton>Get Started</AButton>
+  <AButton>
+    <AIcon path={mdiGithub} size="0.5rem"/>
+    Github
+  </AButton>
+</div>
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
+import path from 'path'
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+		  $lib: path.resolve('./src/lib'),
+		  $assets: path.resolve('./src/assets')
+		}
+	}
 })
