64c514c61d8fe39456e77d5a67cc992f22923fe9
dinghaoran <1006433636@qq.com>
2023-04-17 23:27:35 +0800

    Add AceButton

--- src/lib/Ace/AceButton.svelte
+++ src/lib/Ace/AceButton.svelte
@@ -1,5 +1,34 @@
 <script>
-  import AButton from '../A/AButton.svelte'
+  // import AButton from '../A/AButton.svelte'
 </script>
 
-<AButton on:click on:dblclick>AceButton</AButton>
+<!-- <AButton on:click on:dblclick>AceButton</AButton> -->
+
+<button>
+  <slot></slot>
+</button>
+
+<style>
+  button {
+    background-color: #ABBAC3;
+    color: #FFF;
+    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+    border: 5px solid #ABBAC3;
+    transition: background-color 0.15s;
+    position: relative;
+    cursor: pointer;
+    padding: 6px 12px;
+    font-size: 14px;
+    line-height: 1.42857143;
+  }
+  button:hover {
+    background-color: #8B9AA3;
+  }
+  button:active {
+    top: 1px;
+  }
+  button:focus {
+    outline: -webkit-focus-ring-color auto 5px;
+    outline-offset: -2px;
+  }
+</style>
