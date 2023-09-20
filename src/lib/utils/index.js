28ffce269061b6d3e7f1d83aaa76bb2950d0b742
Phantomlsh <phantomlsh@outlook.com>
2023-09-20 16:23:57 -0500

    Add theme

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,2 +1,3 @@
 export { default as debounce } from './debounce.js'
 export { default as forwardBuilder } from './forwardBuilder.js'
+export { default as theme } from './theme.js'
---
+++ src/lib/utils/theme.js
@@ -0,0 +1,9 @@
+import { writable } from 'svelte/store'
+
+export default = writable({
+  primary: '#3b82f6',
+  info: '#0ea5e9',
+  success: '#22c55e',
+  warning: '#eab308',
+  error: '#ef4444'
+})
