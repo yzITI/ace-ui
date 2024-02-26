b48043a71dce213a804f4c5c7c0ac2b61dc9d2f3
Phantomlsh <root@lsh>
2024-02-26 19:54:52 +0000

    Update AModal

--- src/lib/A/AModal.svelte
+++ src/lib/A/AModal.svelte
@@ -1,40 +1,34 @@
 <!-- AModal
-  className: a-absolute a-left a-right a-top a-bottom
+  class: a-absolute a-left a-right a-top a-bottom
   slot: default
 -->
 <script>
-  export let show = false // binding
-  export let className = ''
-  export let style = ''
-  export let disable = false
-  export let background = 'rgba(0, 0, 0, 0.6)'
-  export let duration = 300
-
+  let { show = false, disable = false, background = 'rgba(0, 0, 0, 0.6)', duration = 300, ...props } = $props()
   import { cubicOut } from 'svelte/easing'
   import { fade, fly } from 'svelte/transition'
   let flyConfig = {}
-  $: { // fly transition direction
-    if (className.match('a-left')) flyConfig = { x: '-100%' }
-    if (className.match('a-right')) flyConfig = { x: '100%' }
-    if (className.match('a-top')) flyConfig = { y: '-100%' }
-    if (className.match('a-bottom')) flyConfig = { y: '100%' }
-  }
+  $effect(() => { // fly transition direction
+    if (props.class.match('a-left')) flyConfig = { x: '-100%' }
+    if (props.class.match('a-right')) flyConfig = { x: '100%' }
+    if (props.class.match('a-top')) flyConfig = { y: '-100%' }
+    if (props.class.match('a-bottom')) flyConfig = { y: '100%' }
+  })
   function close () {
     if (!disable) show = false
   }
 </script>
 
 {#if show}
-  <div class={'a-modal-outer ' + className} {style}>
-    <div transition:fade={{ duration, easing: cubicOut }} class="a-modal-bg" style:background on:click={close} on:keyup={() => {}} role="button" tabindex="0"/>
-    <div transition:fly={{ duration, ...flyConfig }} class="a-modal-inner">
+  <div a="modal-outer" {...props}>
+    <div transition:fade={{ duration, easing: cubicOut }} a="modal-bg" style:background={background} onclick={close} role="button" tabindex="0"/>
+    <div transition:fly={{ duration, ...flyConfig }} a="modal-inner">
       <slot></slot>
     </div>
   </div>
 {/if}
 
 <style>
-  div.a-modal-outer {
+  div[a="modal-outer"] {
     position: fixed;
     top: 0;
     left: 0;
@@ -46,7 +40,7 @@
     justify-content: center;
     overflow: hidden;
   }
-  div.a-modal-bg {
+  div[a="modal-bg"] {
     position: absolute;
     top: 0;
     left: 0;
@@ -54,31 +48,31 @@
     right: 0;
     background: rgba(0, 0, 0, 0.6);
   }
-  div.a-modal-inner {
+  div[a="modal-inner"] {
     position: relative;
     overflow: auto;
   }
   /* features */
-  div.a-modal-outer.a-absolute {
+  div[a="modal-outer"].a-absolute {
     position: absolute;
   }
-  div.a-modal-outer.a-left {
+  div[a="modal-outer"].a-left {
     justify-content: start;
   }
-  div.a-modal-outer.a-right {
+  div[a="modal-outer"].a-right {
     justify-content: end;
   }
-  div.a-modal-outer.a-top {
+  div[a="modal-outer"].a-top {
     align-items: start;
   }
-  div.a-modal-outer.a-bottom {
+  div[a="modal-outer"].a-bottom {
     align-items: end;
   }
-  div.a-modal-outer.a-left > div.a-modal-inner, div.a-modal-outer.a-right > div.a-modal-inner {
+  div[a="modal-outer"].a-left > div[a="modal-inner"], div[a="modal-outer"].a-right > div[a="modal-inner"] {
     height: 100%;
     max-width: 90%;
   }
-  div.a-modal-outer.a-bottom > div.a-modal-inner, div.a-modal-outer.a-top > div.a-modal-inner {
+  div[a="modal-outer"].a-bottom > div[a="modal-inner"], div[a="modal-outer"].a-top > div[a="modal-inner"] {
     width: 100%;
     max-height: 90%;
   }
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -1,13 +1,13 @@
 <script>
   import { AButton, AExpand, AFlex, AIcon, AModal, AAttach, ASwitch, ACheckbox } from '$lib'
   import { mdiAbTesting } from '@mdi/js'
-  let showModal = false
-  let showAttach = false
-  let switchValue = false
+  let showModal = $state(false)
+  let showAttach = $state(false)
+  let switchValue = $state(false)
 </script>
 
 <div style="display: flex; align-items: center; padding: 2rem; background: #fff;">
-  <AButton style="margin: 1rem;" title="hi!" onclick={() => { console.log('hi!') }}>Toggle Modal</AButton>
+  <AButton style="margin: 1rem;" title="hi!" onclick={() => { showModal = !showModal }}>Toggle Modal</AButton>
   <AAttach show={showAttach} style="margin: 1rem;" className="a-left-top">
     <AButton slot="target" class="a-round a-shadow" on:click={() => showAttach = !showAttach}>Toggle attach</AButton>
     <div slot="attach" style="background: gray; color: white; border-radius: 5px; margin: 0.25rem; padding: 0.125rem 0.25rem; font-size: 0.8rem;">
@@ -18,7 +18,7 @@
 </div>
 
 <div style="border: red 1px solid; height: 500px; width: 100%; position: relative;">
-  <AModal bind:show={showModal} className="a-absolute a-left">
+  <AModal bind:show={showModal} class="a-absolute a-left">
     <div style="background: white; width: 300px; min-height: 100%; padding: 2rem; box-sizing: border-box; overflow: auto;">
       {#each (new Array(20)) as k}
         <h1>hi!</h1>
