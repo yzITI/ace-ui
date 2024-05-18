b9ced2395432e64ae3ca569803d5d45e5c4d2912
Phantomlsh <root@lsh>
2024-05-18 16:26:10 +0000

    Fix AIcon min-width

--- src/lib/A/AIcon.svelte
+++ src/lib/A/AIcon.svelte
@@ -3,7 +3,7 @@
   let { path = '', size = '1.5rem', ...props } = $props()
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style:width={size} style:height={size} {...props}>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style:width={size} style:min-width={size} style:height={size} {...props}>
   <path d={path} />
 </svg>
 
